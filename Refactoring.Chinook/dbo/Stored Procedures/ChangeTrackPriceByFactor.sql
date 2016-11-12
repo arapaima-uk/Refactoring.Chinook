cce5edf6bf2bf055d9cec135fdb747a963cf3279
Gavin Campbell <gavin@arapaima.uk>
2016-11-12 10:21:43 +0100

    Finished renaming columns

--- Refactoring.Chinook/dbo/Stored Procedures/ChangeTrackPriceByFactor.sql
+++ Refactoring.Chinook/dbo/Stored Procedures/ChangeTrackPriceByFactor.sql
@@ -2,6 +2,6 @@
 	@TrackID int,
 	@Factor NUMERIC(10, 2)
 AS
-	UPDATE Track SET UnitPrice *= @Factor WHERE TrackId = @TrackID;
+	UPDATE Track SET UnitPrice *= @Factor WHERE Track_Id = @TrackID;
 
 RETURN 0
